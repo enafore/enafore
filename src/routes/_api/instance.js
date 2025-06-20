d81c6590cffd3d72719b5eb4b33b3d7c467228c2
Sqx. Flann <fl4nn+git@opensuse.org>
2025-06-20 15:26:54 +0200

    Fall back to instance API v1 if v2 is unavailable

--- src/routes/_api/instance.js
+++ src/routes/_api/instance.js
@@ -2,10 +2,14 @@ import { get, DEFAULT_TIMEOUT } from '../_utils/ajax.js'
 import { auth, basename } from './utils.js'
 
 export async function getInstanceInfo (instanceName, accessToken) {
-  const url = `${basename(instanceName)}/api/v2/instance`
   // accessToken is required in limited federation mode, but elsewhere we don't need it (e.g. during login)
   const headers = accessToken ? auth(accessToken) : null
-  const instance = await get(url, headers, { timeout: DEFAULT_TIMEOUT })
+  let instance
+  try {
+    instance = await get(`${basename(instanceName)}/api/v2/instance`, headers, { timeout: DEFAULT_TIMEOUT })
+  } catch (e) {
+    instance = await get(`${basename(instanceName)}/api/v1/instance`, headers, { timeout: DEFAULT_TIMEOUT })
+  }
   instance.nodeInfo = null
   try {
     const nodeInfo = await get(`${basename(instanceName)}/.well-known/nodeinfo`, headers, { timeout: DEFAULT_TIMEOUT })
